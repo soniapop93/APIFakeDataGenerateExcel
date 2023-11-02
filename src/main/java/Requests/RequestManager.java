d70203ad4cd6cd3b3433ffefcf3cdebeadc37d82
soniapop93 <popsonia85@gmail.com>
2023-11-02 22:54:20 +0200

    implemented classes to get request for every item and started to build the parse of json part

--- src/main/java/Requests/RequestManager.java
+++ src/main/java/Requests/RequestManager.java
@@ -1,5 +1,8 @@
 package Requests;
 
+import Addresses.Address;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -7,7 +10,18 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class RequestManager {
-    public String getRequest(String endpoint) {
+
+
+    private final String endpoint = "https://random-data-api.com/api/v2";
+    private final String addressEndpoint = endpoint + "/addresses";
+    private final String userEndpoint = endpoint + "/users";
+    private final String applianceEndpoint = endpoint + "/appliances";
+    private final String bankEndpoint = endpoint + "/banks";
+    private final String beerEndpoint = endpoint + "beers";
+    private final String bloodTypeEndpoint = endpoint + "blood_type";
+    private final String creditCardEndpoint = endpoint + "credit_cards";
+
+    private String getRequest(String endpoint) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endpoint)).build();
 
@@ -25,5 +39,64 @@ public class RequestManager {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public Address getAddress() {
+        String response = getRequest(addressEndpoint);
+
+        if (response != null) {
+            Gson gson = new Gson();
+            Address address = gson.fromJson(response, Address.class);
+        }
+
+        return address;
+    }
+
+    public void getAppliance() {
+        String response = getRequest(applianceEndpoint);
+
+        if (response != null) {
+            //TODO: implement deserialization
+        }
+    }
+
+    public void getBank() {
+        String response = getRequest(bankEndpoint);
+
+        if (response != null) {
+            //TODO: implement deserialization
+        }
+    }
+
+    public void getBeer() {
+        String response = getRequest(beerEndpoint);
+
+        if (response != null) {
+            //TODO: implement deserialization
+        }
+    }
+
+    public void getBloodType() {
+        String response = getRequest(bloodTypeEndpoint);
+
+        if (response != null) {
+            //TODO: implement deserialization
+        }
+    }
+
+    public void getCreditCard() {
+        String response = getRequest(creditCardEndpoint);
+
+        if (response != null) {
+            //TODO: implement deserialization
+        }
+    }
+
+    public void getUser() {
+        String response = getRequest(userEndpoint);
+
+        if (response != null) {
+            //TODO: implement deserialization
+        }
     }
 }
